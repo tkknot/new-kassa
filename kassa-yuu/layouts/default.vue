cd3e2e22ba87cdedf628a0d3c34eeffabf9c3cb3
tkknot <takeda19991015@gmail.com>
2020-12-21 16:12:44 +0900

    save

--- kassa-yuu/layouts/default.vue
+++ kassa-yuu/layouts/default.vue
@@ -1,7 +1,15 @@
 <template>
-  <div>
-    
-  </div>
+  <v-app>
+    <headerComponent />
+
+    <v-main>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+
+    <footerComponent />
+  </v-app>
 </template>
 
 <style lang="">
--- kassa-yuu/nuxt.config.js
+++ kassa-yuu/nuxt.config.js
@@ -8,10 +8,17 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: "温かっさトリートメントサロン「かっさyuu」。小顔ケアで有名なかっさは全身にも使えます。東洋医学的デトックスでリラクゼーションの一歩先へ。" }
+
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: "https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;500;700&family=Noto+Serif+JP:wght@300;400;500;600;700&display=swap"},
+    ],
+    script: [
+      {src:"https://kit.fontawesome.com/52d0ef7a7b.js"},
+      {src:"https://code.jquery.com/jquery-3.5.1.js"},
+      {src:"https://maps.googleapis.com/maps/api/js?language=ja&region=JP&key=AIzaSyDDrALEOJ2wXMB6331gtCF_tgFEVzq4DcM&callback=initMap&libraries=places",}
     ]
   },
 
